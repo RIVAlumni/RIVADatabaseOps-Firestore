5e181cd8940b84dee7605205f9037417032d2494
Aaron Teo <aaron.teo@riv-alumni.com>
2020-06-19 16:03:59 +0800

    Added 2 more aggregation rules

--- tests/anonymous_aggregations.test.ts
+++ tests/anonymous_aggregations.test.ts
@@ -1,15 +1,13 @@
 import { setup, teardown } from ".."
 import { firestore } from "@firebase/testing"
 
-import data from "../utils/predefinedData"
-
 describe("User Aggregation Tests", () => {
   let db: firestore.Firestore
 
   beforeEach(async () => (db = await setup(undefined)))
   afterEach(async () => await teardown())
 
-  it("should deny reading/writing/deleting the users aggregation document", async () => {
+  it("should deny reading/writing/deleting users aggregation document", async () => {
     const ref = db.doc("aggregations/users")
 
     await expect(ref.get()).toDeny()
@@ -18,8 +16,26 @@ describe("User Aggregation Tests", () => {
     await expect(ref.delete()).toDeny()
   })
 
-  it("should deny reading/writing/deleting the members aggregation document", async () => {
+  it("should deny reading/writing/deleting members aggregation document", async () => {
     const ref = db.doc("aggregations/members")
+
+    await expect(ref.get()).toDeny()
+    await expect(ref.set({})).toDeny()
+    await expect(ref.update({})).toDeny()
+    await expect(ref.delete()).toDeny()
+  })
+
+  it("should deny reading/writing/deleting events aggregation document", async () => {
+    const ref = db.doc("aggregations/events")
+
+    await expect(ref.get()).toDeny()
+    await expect(ref.set({})).toDeny()
+    await expect(ref.update({})).toDeny()
+    await expect(ref.delete()).toDeny()
+  })
+
+  it("should deny reading/writing/deleting participations aggregation document", async () => {
+    const ref = db.doc("aggregations/participations")
 
     await expect(ref.get()).toDeny()
     await expect(ref.set({})).toDeny()
